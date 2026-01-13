b07f3d2a67ade7fdae889c5112abb5a5192a6187
Jidneshp <jidneshpatil866@gmail.com>
2026-01-13 13:22:11 +0530

    logger file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from src.logger import logging
 import sys
 from src.exception import CustomException
 
 if __name__ == "__main__":
     try:
+        logging.info('Logging has started')
         a = 10
         b = 0
         print(a/b)
